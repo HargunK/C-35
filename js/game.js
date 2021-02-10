72628ce323fe46e69ce35054c134ef0b87e4454f
HargunK <74647406+HargunK@users.noreply.github.com>
2021-02-10 15:54:55 +0530

    Add files via upload

---
+++ js/game.js
@@ -0,0 +1,29 @@
+class Game{
+constructor(){}
+
+
+getState(){
+    var ref1 = database.ref('gameState');
+    ref1.on("value", function(data){
+        gameState : data.val();
+    })
+}
+
+update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+
+
+
+}
